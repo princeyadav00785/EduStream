08358f0bd1183de176d46827c222ad62829b425f
Prince Yadav <princeyadav00785@gmail.com>
2024-12-18 15:48:44 +0530

    fixing minor padding

---
+++ edustream-frontend/src/components/about/about.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-gray-950 ">
+       <div className=" pt-32 max-w-xl mx-auto px-4 py-10 pb-12 ">
+      <h1 className="text-3xl font-bold text-center text-gray-300 mb-6">About Us</h1>
+      <p className="text-lg text-gray-300 leading-relaxed mb-6">
+        EduStream is a premier platform that provides high-quality educational
+        content for learners worldwide. Our mission is to make education
+        accessible to everyone, regardless of their background or location.
+      </p>
+
+      <p className="text-lg text-gray-300 leading-relaxed mb-6">
+        We offer various courses, tutorials, and resources to help learners
+        develop skills in various fields. Whether you're looking to advance your
+        career or explore a new subject, EduStream has something for you.
+      </p>
+
+      <h2 className="text-2xl font-semibold text-white mt-6">Our Mission</h2>
+      <p className="text-lg text-gray-300 leading-relaxed">
+        Our mission is to empower learners with the tools they need to succeed
+        in their academic and professional journeys. We strive to create an
+        inclusive and supportive learning environment for individuals from all
+        walks of life.
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default About;
--- edustream-frontend/src/components/homepage/hero1.tsx
+++ edustream-frontend/src/components/homepage/hero1.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
 import { BackgroundBeams } from "../ui/background-beams";
-
+import Link from "next/link";
 export function Hero1() {
   const words = [
     {
@@ -26,19 +26,22 @@ export function Hero1() {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center lg:mt-32">
         <BackgroundBeams/>
       <p className="text-neutral-600 dark:text-neutral-200 text-base sm:text-lg  max-w-4xl mx-10">
       "Revolutionize Learning with Edustream – Where Live Streaming Meets Real-Time Interaction!"
       </p>
       <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-40 h-10 rounded-xl bg-blue-700 border dark:border-white border-transparent text-white text-sm">
+      <div className=" z-[5] flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+       <Link href="/auth/login">
+       <button className="w-40 h-10 rounded-xl bg-blue-700 border dark:border-white border-transparent text-white text-sm">
           Join now
         </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+       </Link>
+        <Link href="/auth/register"><button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
           Signup
         </button>
+        </Link>
       </div>
       <div className="mt-28 w-3/5 h-1/5">
         <img className="rounded-3xl shadow-2xl shadow-white drop-shadow-xl" src="i6.jpg" />
