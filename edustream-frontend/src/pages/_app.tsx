5802a97e1d3e312cad99a9c22f91e371baedec0c
Prince Yadav <princeyadav00785@gmail.com>
2025-03-23 17:12:02 +0530

    update_app_name_fav-icon

--- edustream-frontend/src/pages/_app.tsx
+++ edustream-frontend/src/pages/_app.tsx
@@ -4,11 +4,15 @@ import "@/styles/globals.css";
 import { ToastContainer } from "react-toastify";
 
 import AuthGuard from "@/components/auth/AuthGuard";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
     <Provider store={store}>
       <AuthGuard>
+        <Head>
+          <title>Edustream</title>
+        </Head>
         <Component {...pageProps} />
         <ToastContainer />
       </AuthGuard>
